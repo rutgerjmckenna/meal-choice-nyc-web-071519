4f1e095dd8f81a13fd92efd90007b709edaac091
Rutger McKenna <rutgerjmckenna@gmail.com>
2019-07-15 20:59:28 +0000

    Automatically backed up by Learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,5 +1,5 @@
 # Define a method meal_choice that returns the meal_choice that was
 # passed into it and defaults to meat.
 def meal_choice(what_to_have = "meat")
-  meal_choice(what_to_have)
+  what_to_have
 end
